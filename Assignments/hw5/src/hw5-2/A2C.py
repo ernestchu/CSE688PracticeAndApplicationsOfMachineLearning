1ee706cf33317a63d5ef89da8e1d372457578663
ernestchu <b073040018@nsysu.edu.tw>
2021-05-27 01:34:10 +0800

    hw5-2

--- Assignments/hw5/src/hw5-2/A2C.py
+++ Assignments/hw5/src/hw5-2/A2C.py
@@ -15,9 +15,12 @@ tf.keras.backend.set_floatx('float64')
 parser = argparse.ArgumentParser()
 parser.add_argument('--gamma', type=float, default=0.9)
 parser.add_argument('--log_interval', type=int, default=10000)
+parser.add_argument('--max_updates', type=int, default=10000)
+parser.add_argument('--batch_size', type=int, default=64)
 parser.add_argument('--actor_lr', type=float, default=0.0005)
 parser.add_argument('--critic_lr', type=float, default=0.001)
 parser.add_argument('--save_weights', type=str, required=True)
+parser.add_argument('--load_weights', type=str, default="")
 parser.add_argument('--save_figure', type=str, required=True)
 
 args = parser.parse_args()
@@ -30,6 +33,9 @@ class Agent:
         self.actor = Actor(self.state_dim, self.action_dim, args.actor_lr)
         self.critic = Critic(self.state_dim, args.critic_lr)
         self.gamma = args.gamma
+        
+        if args.load_weights:
+            self.actor.model.load_weights(args.load_weights)
         
         # initialize video system only
         self.env.reset()
@@ -58,8 +64,10 @@ class Agent:
             batch = np.append(batch, elem, axis=0)
         return batch
 
-    def train(self, max_updates=10000, batch_size=64):
-        episode_rewards = []
+    def train(self, max_updates=100, batch_size=64):
+        episode_reward_list = []
+        episode_length_list = []
+        snake_length_list   = []
         actor_loss  = 0
         critic_loss = 0
         for up in tqdm(range(max_updates)):
@@ -67,7 +75,9 @@ class Agent:
             action_list  = []
             reward_list  = []
             done_list    = []
-            step_rewards = []
+            step_reward_list  = []
+            step_snake_length = []
+            
             state        = self.env.reset()
 
             for ba in range(batch_size):
@@ -77,20 +87,24 @@ class Agent:
                 probs  = tf.nn.softmax(self.actor.model(np.expand_dims(state, 0))[0])
                 action = np.random.choice(self.action_dim, p=probs)
 
-                next_state, reward, done, _ = self.env.step(action)
-                step_rewards.append(reward)
+                next_state, reward, done, info = self.env.step(action)
+                step_reward_list.append(reward)
+                step_snake_length.append(info['length'])
                 
                 if done:
                     # the end of an episode
-                    episode_rewards.append(sum(step_rewards)/len(step_rewards))
-                    n_episode = len(episode_rewards)
+                    episode_length_list.append(len(step_reward_list))
+                    episode_reward_list.append(sum(step_reward_list)/len(step_reward_list))
+                    snake_length_list.append(sum(step_snake_length)/len(step_snake_length))
+                    
+                    n_episode = len(episode_reward_list)
                     if n_episode % args.log_interval == 0:
-                        print(f'\nEpisode: {n_episode}, Avg Reward: {episode_rewards[-1]}')
+                        print(f'\nEpisode: {n_episode}, Avg Reward: {episode_reward_list[-1]}')
                         
-                    step_rewards = []
+                    step_reward_list = []
                     next_state   = self.env.reset()
                     
-                    if max(episode_rewards) == episode_rewards[-1]:
+                    if max(episode_reward_list) == episode_reward_list[-1]:
                         self.actor.model.save_weights(args.save_weights)
 
                 # make single batches
@@ -128,12 +142,26 @@ class Agent:
             critic_loss = self.critic.train(states, returns)
             
         # save figure
-        plt.plot(episode_rewards)
+        mean_n = 100
+        n_episode = len(episode_reward_list)
+        
+        episode_reward_list = [sum(episode_reward_list[l:l+mean_n])/mean_n for l in range(0, n_episode, mean_n)]
+        episode_length_list = [sum(episode_length_list[l:l+mean_n])/mean_n for l in range(0, n_episode, mean_n)]
+        snake_length_list   = [sum(snake_length_list[l:l+mean_n])/mean_n   for l in range(0, n_episode, mean_n)]
+        
+        x = np.linspace(0, n_episode, len(episode_reward_list))
+        
+        plt.plot(x, episode_reward_list, label='Mean 100-Episode Reward')
+        plt.plot(x, snake_length_list, label='Mean 100-Episode Snake Length')
+        plt.plot(x, episode_length_list, label='Mean 100-Episode Episode Length')
+        plt.legend()
+        plt.xlabel('Episode')
+        plt.title('A2C-snake_env')
         plt.savefig(args.save_figure)
 
 def main():
     agent = Agent()
-    agent.train()
+    agent.train(args.max_updates, args.batch_size)
 
 
 if __name__ == "__main__":
